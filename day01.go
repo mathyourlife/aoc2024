3ab3d389702025718c87440f6f1635959e0b19dc
Dan Couture <mathyourlife@gmail.com>
2024-12-03 19:10:33 -0500

    adding problem for day 1

--- day01.go
+++ day01.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+// This time, you'll need to figure out exactly how often each number from the left list appears
+// in the right list. Calculate a total similarity score by adding up each number in the left list
+// after multiplying it by the number of times that number appears in the right list.
 func Day01Part2(scanner *bufio.Scanner) int {
 	var lines [][]int
 	lines = parse(scanner, func(line string) []int {
@@ -32,6 +35,12 @@ func Day01Part2(scanner *bufio.Scanner) int {
 	return total
 }
 
+// Pair up the smallest number in the left list with the smallest number in the right
+// list, then the second-smallest left number with the second-smallest right number, and so on.
+//
+// Within each pair, figure out how far apart the two numbers are; you'll need to add up all of
+// those distances. For example, if you pair up a 3 from the left list with a 7 from the right
+// list, the distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
 func Day01Part1(scanner *bufio.Scanner) int {
 	var lines [][]int
 	lines = parse(scanner, func(line string) []int {
